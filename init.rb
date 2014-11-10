524a7f5fbcfa97e0a1ffe75d895c68d468fb480c
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-11-10 14:00:09 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :toggl_timer do
+  name 'Toggl Timer plugin'
+  author 'Jacek Hiszpanski'
+  description 'Toggle integration with redmine'
+  version '0.0.1'
+  url 'https://github.com/hipek/toggl_timer'
+  author_url 'https://github.com/hipek'
+end
